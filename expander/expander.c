96a39b0fc4ae39ce7e9351a5054afb9175f6dabf
Gabriel Danis <gabriel.danis@gmail.com>
2023-12-19 19:10:26 +0100

    sub linked list working properly when expanding vars

--- expander/expander.c
+++ expander/expander.c
@@ -6,21 +6,30 @@
 /*   By: gdanis <gdanis@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/19 15:04:02 by gdanis            #+#    #+#             */
-/*   Updated: 2023/12/19 18:30:21 by gdanis           ###   ########.fr       */
+/*   Updated: 2023/12/19 19:08:37 by gdanis           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../header/minishell.h"
 
+void	set_sq_flag(int *sq_flag, char c)
+{
+	if (c == 39)	
+	{
+		if (!(*sq_flag))
+			*sq_flag = 1;
+		else
+			*sq_flag = 0;
+	}
+}
+
 void	expand_vars(t_parsed *plist)
 {
 	t_parsed	*tmp;
 	int	sq_flag;
-	int	d_flag;
 	int	i;
 
 	sq_flag = 0;
-	d_flag = 0;
 	i =0;
 	while (plist->str[i])
 	{
@@ -29,30 +38,25 @@ void	expand_vars(t_parsed *plist)
 				free_and_exit(MALLOC_ERROR);
 		*tmp = (t_parsed){0};
 		plist_add_to_last_ex(&tmp, &plist);
-		if (!d_flag)
+		if (plist->str[i] != '$')
 		{
-			while (plist->str[i] && !d_flag)
+			while (plist->str[i])
 			{
-				if (plist->str[i] == 39)	
-				{
-					if (!sq_flag)
-						sq_flag = 1;
-					else
-						sq_flag = 0;
-				}
 				if (plist->str[i] == '$' && !sq_flag)
-					d_flag = 1;
+					break ;
+				set_sq_flag(&sq_flag, plist->str[i]);
 				ft_charjoin(&(p_lstlast(plist->ex)->str), plist->str[i]);
 				i++;
 			}
 		}
 		else
 		{
-			while (plist->str[i] && check_is_var(plist->str[i]))	
+			while (plist->str[i])	
 			{
-				d_flag = 0;
 				ft_charjoin(&(p_lstlast(plist->ex)->str), plist->str[i]);
 				i++;
+				if (!check_is_var(plist->str[i]))
+					break ;
 			}
 		}
 	}
